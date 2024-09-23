62941cf25eb73f6e6835aa27edfc91be76ddc979
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2024-09-23 18:51:46 +0200

    Adapt for experiment

--- examples/locust/locustfile.py
+++ examples/locust/locustfile.py
@@ -1,7 +1,8 @@
+import json
 import logging
 from random import randint, choice
 
-from locust import HttpUser, task
+from locust import HttpUser, task, LoadTestShape, events
 
 # logging
 logging.getLogger().setLevel(logging.INFO)
@@ -15,7 +16,7 @@ class UserBehavior(HttpUser):
         Simulates user behaviour.
         :return: None
         """
-        logging.info("Starting user.")
+        # logging.info("Starting user.")
         self.visit_home()
         self.login()
         self.browse()
@@ -25,7 +26,7 @@ class UserBehavior(HttpUser):
             self.buy()
         self.visit_profile()
         self.logout()
-        logging.info("Completed user.")
+        # logging.info("Completed user.")
 
     def visit_home(self) -> None:
         """
@@ -35,7 +36,8 @@ class UserBehavior(HttpUser):
         # load landing page
         res = self.client.get('/')
         if res.ok:
-            logging.info("Loaded landing page.")
+            # logging.info("Loaded landing page.")
+            pass
         else:
             logging.error(f"Could not load landing page: {res.status_code}")
 
@@ -47,14 +49,17 @@ class UserBehavior(HttpUser):
         # load login page
         res = self.client.get('/login')
         if res.ok:
-            logging.info("Loaded login page.")
+            # logging.info("Loaded login page.")
+            pass
         else:
             logging.error(f"Could not load login page: {res.status_code}")
+            print(res.text)
         # login random user
         user = randint(1, 99)
         login_request = self.client.post("/loginAction", params={"username": user, "password": "password"})
         if login_request.ok:
-            logging.info(f"Login with username: {user}")
+            # logging.info(f"Login with username: {user}")
+            pass
         else:
             logging.error(
                 f"Could not login with username: {user} - status: {login_request.status_code}")
@@ -71,15 +76,16 @@ class UserBehavior(HttpUser):
             page = randint(1, 5)
             category_request = self.client.get("/category", params={"page": page, "category": category_id})
             if category_request.ok:
-                logging.info(f"Visited category {category_id} on page 1")
+                # logging.info(f"Visited category {category_id} on page 1")
                 # browses random product
                 product_id = randint(7, 506)
                 product_request = self.client.get("/product", params={"id": product_id})
                 if product_request.ok:
-                    logging.info(f"Visited product with id {product_id}.")
+                    # logging.info(f"Visited product with id {product_id}.")
                     cart_request = self.client.post("/cartAction", params={"addToCart": "", "productid": product_id})
                     if cart_request.ok:
-                        logging.info(f"Added product {product_id} to cart.")
+                        # logging.info(f"Added product {product_id} to cart.")
+                        pass
                     else:
                         logging.error(
                             f"Could not put product {product_id} in cart - status {cart_request.status_code}")
@@ -108,7 +114,8 @@ class UserBehavior(HttpUser):
         }
         buy_request = self.client.post("/cartAction", params=user_data)
         if buy_request.ok:
-            logging.info(f"Bought products.")
+            # logging.info(f"Bought products.")
+            pass
         else:
             logging.error("Could not buy products.")
 
@@ -119,7 +126,8 @@ class UserBehavior(HttpUser):
         """
         profile_request = self.client.get("/profile")
         if profile_request.ok:
-            logging.info("Visited profile page.")
+            # logging.info("Visited profile page.")
+            pass
         else:
             logging.error("Could not visit profile page.")
 
@@ -130,6 +138,31 @@ class UserBehavior(HttpUser):
         """
         logout_request = self.client.post("/loginAction", params={"logout": ""})
         if logout_request.ok:
-            logging.info("Successful logout.")
+            # logging.info("Successful logout.")
+            pass
         else:
             logging.error(f"Could not log out - status: {logout_request.status_code}")
+
+
+file_raw_requests = open("raw_requests.csv", "w")
+file_raw_users = open("raw_users.csv", "w")
+
+@events.request.add_listener
+def on_request(*args, **kwargs):
+    file_raw_requests.write(f'{int(kwargs["start_time"]*1_000)},{int(kwargs["start_time"]*1_000) + int(kwargs["response_time"])},{int(kwargs["response_time"])}\n')
+
+# @events.worker_report.add_listener
+# def on_worker_report(*args, **kwargs):
+#     import pdb; pdb.set_trace()
+
+import time
+from locust import runners
+
+@events.spawning_complete.add_listener
+def on_spawning_complete(user_count):
+    file_raw_users.write(f'{time.time()},{user_count}\n')
+
+@events.quit.add_listener
+def on_quit(*args, **kwargs):
+    file_raw_requests.close()
+    file_raw_users.close()
